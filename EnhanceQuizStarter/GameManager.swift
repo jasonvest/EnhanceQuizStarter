1e4e1b1b17d92998cfe65ce25f7c2a5b4347510a
Jason Vest <jason@jasonvest.com>
2019-06-01 12:00:20 -0700

    Update UI and controller
    Updated UI to show correct answer when wrong, refined score messaging.

--- EnhanceQuizStarter/GameManager.swift
+++ EnhanceQuizStarter/GameManager.swift
@@ -15,61 +15,61 @@ class GameManager    {
          "Choice 2": "Franklin D. Roosevelt",
          "Choice 3": "Woodrow Wilson",
          "Choice 4": "Andrew Jackson",
-         "Answer": "Choice 2"],
+         "Answer": "Franklin D. Roosevelt"],
         ["Question": "Which of the following countries has the most residents?",
          "Choice 1": "Nigeria",
          "Choice 2": "Russia",
          "Choice 3": "Iran",
          "Choice 4": "Vietnam",
-         "Answer": "Choice 1"],
+         "Answer": "Nigeria"],
         ["Question": "In what year was the United Nations founded?",
          "Choice 1": "1918",
          "Choice 2": "1919",
          "Choice 3": "1945",
          "Choice 4": "1954",
-         "Answer": "Choice 3"],
+         "Answer": "1945"],
         ["Question": "The Titanic departed from the United Kingdom, where was it supposed to arrive?",
          "Choice 1": "Paris",
          "Choice 2": "Washington D.C.",
          "Choice 3": "New York City",
          "Choice 4": "Boston",
-         "Answer": "Choice 3"],
+         "Answer": "New York City"],
         ["Question": "Which nation produces the most oil?",
          "Choice 1": "Iran",
          "Choice 2": "Iraq",
          "Choice 3": "Brazil",
          "Choice 4": "Canada",
-         "Answer": "Choice 4"],
+         "Answer": "Canada"],
         ["Question": "Which country has most recently won consecutive World Cups in Soccer?",
          "Choice 1": "Italy",
          "Choice 2": "Brazil",
          "Choice 3": "Argentina",
          "Choice 4": "Spain",
-         "Answer": "Choice 2"],
+         "Answer": "Brazil"],
         ["Question": "Which of the following rivers is longest?",
          "Choice 1": "Yangtze",
          "Choice 2": "Mississippi",
          "Choice 3": "Congo",
          "Choice 4": "Mekong",
-         "Answer": "Choice 2"],
+         "Answer": "Mississippi"],
         ["Question": "Which city is the oldest?",
          "Choice 1": "Mexico City",
          "Choice 2": "Cape Town",
          "Choice 3": "San Juan",
          "Choice 4": "Sydney",
-         "Answer": "Choice 1"],
+         "Answer": "Mexico City"],
         ["Question": "Which country was the first to allow women to vote in national elections?",
          "Choice 1": "Poland",
          "Choice 2": "United States",
          "Choice 3": "Sweden",
          "Choice 4": "Senegal",
-         "Answer": "Choice 1"],
+         "Answer": "Poland"],
         ["Question": "Which of these countries won the most medals in the 2012 Summer Games?",
          "Choice 1": "France",
          "Choice 2": "Germany",
          "Choice 3": "Japan",
          "Choice 4": "Great Britian",
-         "Answer": "Choice 4"],
+         "Answer": "Great Britian"],
     ]
     
     let questionsPerRound: Int
@@ -110,14 +110,7 @@ class GameManager    {
         
         return question
     }
-    /*
-    func questionChoices() -> [String] {
-        var results = [String]()
-        for (key, value) in questions[indexOfSelectedQuestion]  {
-            if key.hasPrefix("Choice") && value != ""  {
-                results.append(value)
-            }
-        }
-        return results
-    }*/
+    func scorePercentage() -> Double {
+        return Double(self.correctQuestions)/Double(self.questionsAsked)
+    }
 }
--- EnhanceQuizStarter/ViewController.swift
+++ EnhanceQuizStarter/ViewController.swift
@@ -24,8 +24,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondChoiceButton: UIButton!
     @IBOutlet weak var firstChoiceButton: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var questionResults: UILabel!
+    @IBOutlet weak var correctAnswerLabel: UILabel!
     
+    
+    //Researched on internet and documentation
+    override var preferredStatusBarStyle: UIStatusBarStyle  {
+        return .lightContent
+    }
     override func viewDidLoad() {
+        //Researched on internet and documentation
+        self.setNeedsStatusBarAppearanceUpdate()
         super.viewDidLoad()
         
         loadGameStartSound()
@@ -41,19 +50,32 @@ class ViewController: UIViewController {
         AudioServicesCreateSystemSoundID(soundUrl as CFURL, &gameSound)
     }
     
+    func listOfChoiceButtons() -> [UIButton] {
+        let buttons: [UIButton] = [firstChoiceButton, secondChoiceButton, thirdChoiceButton,fourthChoiceButton]
+        return buttons
+    }
+    
     func playGameStartSound() {
         AudioServicesPlaySystemSound(gameSound)
     }
     
     // Display questions and choices
     func displayQuestion() {
+        questionResults.isHidden = true
+        correctAnswerLabel.isHidden = true
+        
         let question = gameManager.randomQuestion()
         questionField.text = question.question
         
         //Loop through available choices and update buttons appropriately
         let choices = question.choices
-        let buttons: [UIButton] = [firstChoiceButton, secondChoiceButton, thirdChoiceButton,fourthChoiceButton]
-        
+        let buttons = listOfChoiceButtons()
+        for button in buttons   {
+            button.isHidden = true
+            if button.isEnabled == false    {
+                button.isEnabled = true
+            }
+        }
         for index in 0..<choices.count  {
             buttons[index].setTitle(choices[index], for: UIControlState.normal)
             buttons[index].isHidden = false
@@ -64,15 +86,35 @@ class ViewController: UIViewController {
     
     func displayScore() {
         // Hide the answer buttons
-        firstChoiceButton.isHidden = true
-        secondChoiceButton.isHidden = true
-        thirdChoiceButton.isHidden = true
-        fourthChoiceButton.isHidden = true
+        let buttons = listOfChoiceButtons()
+        
+        for button in buttons {
+            button.isHidden = true
+        }
+        
+        // Hide the question results and correct answer labels
+        questionResults.isHidden = true
+        correctAnswerLabel.isHidden = true
         
         // Display play again button
         playAgainButton.isHidden = false
         
-        questionField.text = "Way to go!\nYou got \(gameManager.correctQuestions) out of \(gameManager.questionsPerRound) correct!"
+        // Customize success message based on results
+        let scorePercentage = gameManager.scorePercentage()
+        var message: String
+        switch scorePercentage {
+        case 1.0:
+            message = "Perfect score!\n"
+        case 0.75..<100.0:
+            message = "Not too bad...\n"
+        case 0.50..<0.75:
+            message = "I think you need some practice...\n"
+        case 0.00..<0.50:
+            message = "That was rough!\n"
+        default:
+            message = ""
+        }
+        questionField.text = message + "You got \(gameManager.correctQuestions) out of \(gameManager.questionsPerRound) correct!"
     }
     
     func nextRound() {
@@ -96,41 +138,49 @@ class ViewController: UIViewController {
             self.nextRound()
         }
     }
-    
-    // MARK: - Actions
-    /*
+
     @IBAction func checkAnswer(_ sender: UIButton) {
-        // Increment the questions asked counter
-        triviaProvider.questionsAsked += 1
-        
-        let selectedQuestionDict = triviaProvider.questions[triviaProvider.indexOfSelectedQuestion]
-        let correctAnswer = selectedQuestionDict["Answer"]
-        
-        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
-            triviaProvider.correctQuestions += 1
-            questionField.text = "Correct!"
-        } else {
-            questionField.text = "Sorry, wrong answer!"
+        //Highlight selected button and disable input
+        let buttons = listOfChoiceButtons()
+        for button in buttons   {
+            if button !== sender    {
+                button.isEnabled = false
+            }
         }
- 
+        
+        //Increment the questions asked counter
+        gameManager.questionsAsked += 1
+        let answerSelected = sender.currentTitle!
+        let correctAnswer = gameManager.quiz.questions[gameManager.indexOfSelectedQuestion].answer
+        
+        if answerSelected == correctAnswer {
+            gameManager.correctQuestions += 1
+            questionResults.text = "Correct!"
+            questionResults.textColor = UIColor.init(red: 0, green: 255.0, blue: 0, alpha: 1.0)
+            questionResults.isHidden = false
+            
+        } else  {
+            questionResults.text = "Sorry, wrong answer!"
+            questionResults.textColor = UIColor.init(red: 255.0, green: 0, blue: 0, alpha: 1.0)
+            correctAnswerLabel.text = "Correct answer: \(correctAnswer)"
+            
+            correctAnswerLabel.isHidden = false
+            questionResults.isHidden = false
+        }
         loadNextRound(delay: 2)
-    }*/
-    
+    }
     
     @IBAction func playAgain(_ sender: UIButton) {
-        // Show the answer buttons
-        firstChoiceButton.isHidden = true
-        secondChoiceButton.isHidden = true
-        thirdChoiceButton.isHidden = true
-        fourthChoiceButton.isHidden = true
-        
+        // Reset choice buttons to hidden
+        let buttons = listOfChoiceButtons()
+        for button in buttons   {
+            button.isHidden = true
+        }        
         gameManager.questionsAsked = 0
         gameManager.correctQuestions = 0
         gameManager.questionsUsed = []
         gameManager.indexOfSelectedQuestion = 0
         nextRound()
     }
-    
-
 }
 
